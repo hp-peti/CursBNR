cef8bd82ee153d18bdf84fd76e12a1a63cb41ea3
Hanos-Puskai Péter <hp.peti@gmail.com>
2023-02-03 01:49:26 +0200

    new data and plots

--- cursbnr-py/client2db.py
+++ cursbnr-py/client2db.py
@@ -4,30 +4,56 @@ from dateutil.rrule import rrule, DAILY
 from tqdm import tqdm
 from suds import WebFault
 import re
+
 # %%
 from cursdb import CursDB
-from curstypes import to_date
+from cursclient import CursClient
+from curstypes import CursMap, to_date
 
-db = CursDB("bnr.db")
 
-start_date = "2000-01-01"
+start_date = "1990-01-01"
 commit_every_n = 1024
 before_first_valid_date = {
-    "THB": to_date("2017-06-18")
+    "AED": to_date("2009-03-01"),
+    "BRL": to_date("2009-03-01"),
+    "CNY": to_date("2009-03-01"),
+    "INR": to_date("2009-03-01"),
+    "KRW": to_date("2009-03-01"),
+    "MXN": to_date("2009-03-01"),
+    "NZD": to_date("2009-03-01"),
+    "RSD": to_date("2009-03-01"),
+    "UAH": to_date("2009-03-01"),
+    "ZAR": to_date("2009-03-01"),
+    "BGN": to_date("2007-12-02"),
+    "RUB": to_date("2007-11-11"),
+    "TRY": to_date('2005-01-02'),
+    "CZK": to_date('2001-11-11'),
+    "HUF": to_date('2001-11-11'),
+    "PLN": to_date('2001-11-11'),
+    # "---": to_date('2009-XX-XX'),
+    # "---": to_date('2009-XX-XX'),
+    # "---": to_date('2009-XX-XX'),
+    "THB": to_date("2017-06-18"),
 }
 autoexclude = True
 
 # %%
-from cursclient import CursClient
 
+db = CursDB("bnr.db")
 client = CursClient()
 
 # %%
 currencies = list(map(lambda x: x[1], client.getall()))
 print(*currencies, sep=", ")
 
+xcache = set()
+for date, currency, _ in db.select_rows(currency=currencies):
+    xcache.add((date, currency))
+
 # %%
-days = list(rrule(DAILY, to_date(start_date), until=client.lastdate))
+days = list(
+    map(lambda d: d.date(), rrule(DAILY, to_date(start_date), until=client.lastdate))
+)
 days.reverse()
 loop = tqdm(days, leave=False)
 exclude_currency = []
@@ -35,25 +61,31 @@ try:
     inserted = 0
     for date in loop:
         for currency in currencies:
+            if (date, currency) in xcache:
+                xcache.remove((date, currency))
+                continue
+
             if currency in before_first_valid_date:
-                if date.date() <= before_first_valid_date[currency]:
+                if date <= before_first_valid_date[currency]:
                     exclude_currency.append(currency)
                     del before_first_valid_date[currency]
-                    
-                    continue # inner loop
+
+                    continue  # inner loop
 
             if (value := db.get_value(date, currency)) is None:
-                loop.set_postfix_str(f"{date.date()} {currency}")
+                loop.set_postfix_str(f"{date} {currency}")
 
                 try:
                     value = client.getvalue(date, currency)
                 except WebFault as wf:
-                    tqdm.write(f"{wf!s} @{date.date()} {currency})")
+                    tqdm.write(f"{wf!s} @{date} {currency})")
                     if autoexclude:
-                        if re.fullmatch(r".*Object reference not set to an instance of an object\..*", str(wf)):
-                            tqdm.write(f"Skipping {currency} before {date.date()}")
+                        if re.fullmatch(
+                            r".*Object reference not set to an instance of an object\..*",
+                            str(wf),
+                        ):
+                            tqdm.write(f"Skipping {currency} before {date}")
                             exclude_currency.append(currency)
-
 
                 if value is not None:
                     db.insert_value(date, currency, value, replace=False)
@@ -65,8 +97,11 @@ try:
 
         if exclude_currency:
             for currency in exclude_currency:
-                currencies.remove(currency) 
+                currencies.remove(currency)
             exclude_currency.clear()
+
+            if not currencies:
+                break # outer loop
 
 
 except KeyboardInterrupt:
