a5b141424d79fbdccc640b734daae247073bcaff
Hanos-Puskai Péter <hp.peti@gmail.com>
2023-02-02 23:59:52 +0200

    auto-exclude currencies before exception

--- cursbnr-py/client2db.py
+++ cursbnr-py/client2db.py
@@ -3,16 +3,19 @@ from dateutil.relativedelta import relativedelta
 from dateutil.rrule import rrule, DAILY
 from tqdm import tqdm
 from suds import WebFault
-
+import re
 # %%
 from cursdb import CursDB
 from curstypes import to_date
 
 db = CursDB("bnr.db")
 
-start_date = "2014-01-01"
+start_date = "2000-01-01"
 commit_every_n = 1024
-exclude_currency_before = {"THB": to_date("2017-06-19")}
+before_first_valid_date = {
+    "THB": to_date("2017-06-18")
+}
+autoexclude = True
 
 # %%
 from cursclient import CursClient
@@ -27,18 +30,17 @@ print(*currencies, sep=", ")
 days = list(rrule(DAILY, to_date(start_date), until=client.lastdate))
 days.reverse()
 loop = tqdm(days, leave=False)
-excluded_currency = set()
+exclude_currency = []
 try:
     inserted = 0
     for date in loop:
         for currency in currencies:
-            if currency in excluded_currency:
-                continue
-            else:
-                if currency in exclude_currency_before:
-                    if exclude_currency_before[currency] >= date.date():
-                        excluded_currency.add(currency)
-                        del exclude_currency_before[currency]
+            if currency in before_first_valid_date:
+                if date.date() <= before_first_valid_date[currency]:
+                    exclude_currency.append(currency)
+                    del before_first_valid_date[currency]
+                    
+                    continue # inner loop
 
             if (value := db.get_value(date, currency)) is None:
                 loop.set_postfix_str(f"{date.date()} {currency}")
@@ -47,6 +49,11 @@ try:
                     value = client.getvalue(date, currency)
                 except WebFault as wf:
                     tqdm.write(f"{wf!s} @{date.date()} {currency})")
+                    if autoexclude:
+                        if re.fullmatch(r".*Object reference not set to an instance of an object\..*", str(wf)):
+                            tqdm.write(f"Skipping {currency} before {date.date()}")
+                            exclude_currency.add(currency)
+
 
                 if value is not None:
                     db.insert_value(date, currency, value, replace=False)
@@ -55,6 +62,12 @@ try:
                         loop.set_postfix_str("COMMITING...  ")
                         db.commit()
                         inserted = 0
+
+        if exclude_currency:
+            for currency in exclude_currency:
+                currencies.remove(currency) 
+            exclude_currency.clear()
+
 
 except KeyboardInterrupt:
     pass
